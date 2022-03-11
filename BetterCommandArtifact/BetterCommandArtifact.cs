e236a6312e57e84098501b19cbd2e4a30e6175cc
BooopDev <oscar.wright@hotmail.co.uk>
2022-03-11 22:41:19 +0000

    Initial commit

---
+++ BetterCommandArtifact/BetterCommandArtifact.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using UnityEngine.Networking;
+using PickupIndex = RoR2.PickupIndex;
+using PickupTransmutationManager = RoR2.PickupTransmutationManager;
+
+namespace BetterCommandArtifact
+{
+    [BepInDependency(R2API.R2API.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    
+    public class BetterCommandArtifact : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "Boooooop";
+        public const string PluginName = "BetterCommandArtifact";
+        public const string PluginVersion = "1.0.0";
+
+        public static ConfigFile configFile = new ConfigFile(Paths.ConfigPath + "\\BetterCommandArtifact.cfg", true);
+
+        public static ConfigEntry<int> itemAmount { get; set; }
+        
+        public void OnEnable()
+        {
+            itemAmount = configFile.Bind("BetterCommandArtifact", "itemAmount", 5, new ConfigDescription("Set the amount of items shown when opening a command artifact drop. \n Value must be Greater Than 0."));
+            Config.SettingChanged += ConfigOnSettingChanged;
+            On.RoR2.PickupPickerController.SetOptionsFromPickupForCommandArtifact += SetOptions;
+        }
+
+        void ConfigOnSettingChanged(object sender, SettingChangedEventArgs e)
+        {
+            if (itemAmount.Value <= 0)
+                itemAmount.Value = 1;
+        }
+
+        public void OnDisable()
+        {
+            On.RoR2.PickupPickerController.SetOptionsFromPickupForCommandArtifact -= SetOptions;
+        }
+        
+        [Server]
+        void SetOptions(On.RoR2.PickupPickerController.orig_SetOptionsFromPickupForCommandArtifact orig, RoR2.PickupPickerController self, PickupIndex pickupIndex)
+        {
+            if (!NetworkServer.active) return;
+            
+            PickupIndex[] newSelection = PickupTransmutationManager.GetGroupFromPickupIndex(pickupIndex);
+            PickupPickerController.Option[] array;
+
+            if (newSelection == null)
+            {
+                array = new PickupPickerController.Option[1]
+                {
+                    new PickupPickerController.Option
+                    {
+                        available = true,
+                        pickupIndex = pickupIndex
+                    }
+                };
+            }
+            else
+            {
+                Random rnd = new Random();
+                List<PickupIndex> list = (from x in newSelection.ToList() orderby rnd.Next() select x).Take(itemAmount.Value).ToList();
+                array = new PickupPickerController.Option[list.Count];
+                for (int i = 0; i < list.Count; i++)
+                {
+                    PickupIndex pickupIndex2 = list[i];
+                    array[i] = new PickupPickerController.Option
+                    {
+                        available = Run.instance.IsPickupAvailable(pickupIndex2),
+                        pickupIndex = pickupIndex2
+                    };
+                }
+            }
+            self.SetOptionsServer(array);
+        }
+    }
+}
