e7fd865b52cb8b50ca390b55f51f193c62355df9
Moffein <22742925+Moffein@users.noreply.github.com>
2024-07-20 22:00:51 -0700

    Added tier config

--- BetterCommandArtifact/BetterCommandArtifact.cs
+++ BetterCommandArtifact/BetterCommandArtifact.cs
@@ -30,7 +30,7 @@ namespace BetterCommandArtifact
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "Boooooop";
         public const string PluginName = "BetterCommandArtifact";
-        public const string PluginVersion = "1.3.0";
+        public const string PluginVersion = "1.4.0";
 
         public static ConfigFile configFile = new ConfigFile(Paths.ConfigPath + "\\BetterCommandArtifact.cfg", true);
 
@@ -39,13 +39,47 @@ namespace BetterCommandArtifact
 
         public static ConfigEntry<bool> enablePrinter { get; set; }
 
+        public static ConfigEntry<bool> perTierEnabled { get; set; }
+
+        public static ConfigEntry<int> whiteAmount { get; set; }
+        public static ConfigEntry<int> greenAmount { get; set; }
+        public static ConfigEntry<int> redAmount { get; set; }
+        public static ConfigEntry<int> yellowAmount { get; set; }
+
+        public static ConfigEntry<int> equipmentAmount { get; set; }
+        public static ConfigEntry<int> equipmentLunarAmount { get; set; }
+
+        public static ConfigEntry<int> whiteVoidAmount { get; set; }
+        public static ConfigEntry<int> greenVoidAmount { get; set; }
+        public static ConfigEntry<int> redVoidAmount { get; set; }
+        public static ConfigEntry<int> yellowVoidAmount { get; set; }
+
+        public static ConfigEntry<int> lunarAmount { get; set; }
+
         public void OnEnable()
         {
             itemAmount = configFile.Bind("BetterCommandArtifact", "itemAmount", 3, new ConfigDescription("Set the amount of items shown when opening a command artifact drop. \n Value must be Greater Than 0."));
-            allowBoss = configFile.Bind("BetterCommandArtifact", "Allow Boss", false, new ConfigDescription("Allow boss items to have multiple options?"));
+            allowBoss = configFile.Bind("BetterCommandArtifact", "Allow Boss", false, new ConfigDescription("Allow boss items to have multiple options? \n Ignored if Per-tier config is enabled."));
             enablePrinter = configFile.Bind("BetterCommandArtifact", "Enable Printers and Scrappers", true, new ConfigDescription("Allow printers and scrappers to spawn while Command is enabled? Vanilla is false."));
+            perTierEnabled = configFile.Bind("BetterCommandArtifact", "Use Per-tier Config", false, new ConfigDescription("Enables the per-tier section of the config."));
 
-            Config.SettingChanged += ConfigOnSettingChanged;
+            whiteAmount = configFile.Bind("Tier Settings", "T1 White", 3, new ConfigDescription("How many options this tier has."));
+            whiteVoidAmount = configFile.Bind("Tier Settings", "T1 White (Void)", 3, new ConfigDescription("How many options this tier has."));
+            
+            greenAmount = configFile.Bind("Tier Settings", "T2 Green", 3, new ConfigDescription("How many options this tier has."));
+            greenVoidAmount = configFile.Bind("Tier Settings", "T2 Green (Void)", 3, new ConfigDescription("How many options this tier has."));
+
+            redAmount = configFile.Bind("Tier Settings", "T3 Red", 3, new ConfigDescription("How many options this tier has."));
+            redVoidAmount = configFile.Bind("Tier Settings", "T3 Red (Void)", 3, new ConfigDescription("How many options this tier has."));
+
+            yellowAmount = configFile.Bind("Tier Settings", "Yellow", 1, new ConfigDescription("How many options this tier has."));
+            yellowVoidAmount = configFile.Bind("Tier Settings", "Yellow (Void)", 1, new ConfigDescription("How many options this tier has."));
+
+            equipmentAmount = configFile.Bind("Tier Settings", "Equipment", 3, new ConfigDescription("How many options this tier has."));
+
+            lunarAmount = configFile.Bind("Tier Settings", "Lunar", 3, new ConfigDescription("How many options this tier has."));
+            equipmentLunarAmount = configFile.Bind("Tier Settings", "Lunar Equipment", 3, new ConfigDescription("How many options this tier has."));
+
             On.RoR2.PickupPickerController.SetOptionsFromPickupForCommandArtifact += SetOptions;
             On.RoR2.Artifacts.CommandArtifactManager.OnGenerateInteractableCardSelection += CommandArtifactManager_OnGenerateInteractableCardSelection;
         }
@@ -53,12 +87,6 @@ namespace BetterCommandArtifact
         private void CommandArtifactManager_OnGenerateInteractableCardSelection(On.RoR2.Artifacts.CommandArtifactManager.orig_OnGenerateInteractableCardSelection orig, SceneDirector sceneDirector, DirectorCardCategorySelection dccs)
         {
             if (!enablePrinter.Value) orig(sceneDirector, dccs);
-        }
-
-        void ConfigOnSettingChanged(object sender, SettingChangedEventArgs e)
-        {
-            if (itemAmount.Value <= 0)
-                itemAmount.Value = 1;
         }
 
         public void OnDisable()
@@ -91,6 +119,7 @@ namespace BetterCommandArtifact
                 List<PickupIndex> list = new List<PickupIndex>();
 
                 int extraItems = itemAmount.Value;
+
                 if (pickupIndex != PickupIndex.none)
                 {
                     PickupDef pd = PickupCatalog.GetPickupDef(pickupIndex);
@@ -106,9 +135,63 @@ namespace BetterCommandArtifact
                             if (isValidItem)
                             {
                                 ItemDef id = ItemCatalog.GetItemDef(pd.itemIndex);
-                                if (id != null && (id.deprecatedTier == ItemTier.Boss || id.deprecatedTier == ItemTier.VoidBoss) && !allowBoss.Value)
+                                if (!perTierEnabled.Value)
                                 {
-                                    extraItems = 0;
+                                    if (id != null && (id.deprecatedTier == ItemTier.Boss || id.deprecatedTier == ItemTier.VoidBoss) && !allowBoss.Value)
+                                    {
+                                        extraItems = 0;
+                                    }
+                                }
+                                else
+                                {
+                                    switch (id.deprecatedTier)
+                                    {
+                                        case ItemTier.Tier1:
+                                            extraItems = whiteAmount.Value;
+                                            break;
+                                        case ItemTier.Tier2:
+                                            extraItems = greenAmount.Value;
+                                            break;
+                                        case ItemTier.Tier3:
+                                            extraItems = redAmount.Value;
+                                            break;
+                                        case ItemTier.Boss:
+                                            extraItems = yellowAmount.Value;
+                                            break;
+                                        case ItemTier.VoidTier1:
+                                            extraItems = whiteVoidAmount.Value;
+                                            break;
+                                        case ItemTier.VoidTier2:
+                                            extraItems = greenVoidAmount.Value;
+                                            break;
+                                        case ItemTier.VoidTier3:
+                                            extraItems = redVoidAmount.Value;
+                                            break;
+                                        case ItemTier.VoidBoss:
+                                            extraItems = yellowVoidAmount.Value;
+                                            break;
+                                        case ItemTier.Lunar:
+                                            extraItems = lunarAmount.Value;
+                                            break;
+                                        default:
+                                            //Redundant, but here so that -1 doesn't need to be added to every case.
+                                            extraItems = itemAmount.Value;
+                                            break;
+                                    }
+                                    extraItems--;
+                                }
+                            }
+                            else if (isValidEquip && perTierEnabled.Value)
+                            {
+                                EquipmentDef ed = EquipmentCatalog.GetEquipmentDef(pd.equipmentIndex);
+
+                                if (ed.isLunar)
+                                {
+                                    extraItems = equipmentLunarAmount.Value - 1;
+                                }
+                                else
+                                {
+                                    extraItems = equipmentAmount.Value - 1;
                                 }
                             }
                         }
